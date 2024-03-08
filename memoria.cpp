bb51e1b70cc63fe5e48925df1f282fe9ef40abbc
Josue-gael-guzman-chavez <160081306+Josue-gael-guzman-chavez@users.noreply.github.com>
2024-03-08 02:05:36 +0000

    version 5

---
+++ memoria.cpp
@@ -0,0 +1,116 @@
+#include <iostream>
+using namespace std;
+
+class Personaje
+{
+private:
+    
+public:
+    Personaje() {}
+    ~Personaje() {}
+    void Hablar(){
+        cout<<"Hola enanx"<<endl;
+    }
+};
+
+class Duende
+{
+private:
+    int salud;
+public:
+    Duende() {}
+    ~Duende() {}
+};
+
+class Enano : public Personaje
+{
+private:
+    float altura;
+    int edad;
+    int vida;
+    float altura;
+    int edad;
+
+public:
+    Enano() {}
+    ~Enano() {}
+    void Inicializar(){
+        this->altura = 2;
+        this->edad = 150;
+        this->
+    }
+
+};
+
+int main(int argc, char const *argv[]){
+
+int a =5;
+bool b =true;
+float f = 0.1;
+double d = 0.1;
+char c = 'a';
+Enano e;
+
+cout<<"Tamaño tipos de datos masivo: "<<endl;
+cout<<"Tamaño int:"<<sizeof(int)<<" bytes."<<endl;
+cout<<"Tamaño bool:"<<sizeof(bool)<<" bytes."<<endl;
+cout<<"Tamaño float:"<<sizeof(float)<<" bytes."<<endl;
+cout<<"Tamaño double:"<<sizeof(double)<<" bytes."<<endl;
+cout<<"Tamaño char:"<<sizeof(char)<<" bytes."<<endl;
+cout<<"Tamaño Enano:"<<sizeof(Enano)<<" bytes."<<endl;
+
+cout<<endl;
+cout<<"Memoria Estatica C/C++:"<<endl;
+cout<<"Tamaño int:"<<sizeof(int)<<" bytes."<<endl;
+cout<<"Tamaño bool:"<<sizeof(int)<<" bytes."<<endl;
+cout<<"Tamaño float:"<<sizeof(int)<<" bytes."<<endl;
+cout<<"Tamaño double:"<<sizeof(int)<<" bytes."<<endl;
+cout<<"Tamaño char:"<<sizeof(int)<<static_cast<void*>" bytes."<<endl;
+cout<<"Tamaño Enano:"<<sizeof(int)<<" bytes."<<endl;
+
+
+cout<<endl;
+cout<<"Tamaño "<<sizeof(int)<<" bytes."<<endl;
+cout<<"Direccion Dinamica:"<<malloc(2)<<endl;
+cout<<"Direccion Dinamica int"<<malloc(sizeof(int))<<endl;
+
+//casteo de punteros en C
+Enano* direccion =(Enano*) malloc(sizeof(Enano));
+direccion->Inicializar();
+
+//punteros en C++
+Enano* instancia = new Enano;
+instancia
+
+//operador
+instancia->Comer();
+instancia->Hablar();
+
+//polimorgismo
+Personaje* p = new Duende();
+Personaje* q = new Enano();
+
+p->Hablar();
+q->Hablar();
+
+cout<<"Direccion de p:" <<p<<endl;
+
+//arreglos
+Duende duendes[10];
+cout<<endl;
+cout<<&duendes[0]<<endl;
+cout<<&duendes[1]<<endl;
+cout<<&duendes[2]<<endl;
+cout<<&duendes[3]<<endl;
+cout<<&duendes[4]<<endl;
+
+//convesion a direccion
+cout<<endl;<<duendes+0<<endl;
+cout<<duendes+1<<endl;
+cout<<duendes+2<<endl;
+cout<<duendes+3<<endl;
+cout<<duendes+5<<endl;
+
+
+return 0;
+}
